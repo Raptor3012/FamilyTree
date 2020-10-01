59999e20e8a9a2d5d025710e6f7be846bcf7707a
Raptor3012 <aleksey.shalov@gmail.com>
2020-10-01 13:53:23 +0500

    update

---
+++ Homework1/Homework1/Person.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Numerics;
+
+namespace Homework1
+{
+    partial class Person
+    {
+        public string name { get; }
+        public string sex { get; }
+        public Person parent_1 { set; get; }
+        public Person parent_2 { set; get; }
+        public Person partner { set; get; }
+        public List<Person> childrens = new List<Person>();
+
+        public Person(string name, string sex)
+        {
+            this.name = name;
+            this.sex = sex;
+        }
+        public Person(string name, string sex,Person parent_1, Person parent_2)
+        {
+            this.name = name;
+            this.sex = sex;
+            this.parent_1 = parent_1;
+            this.parent_2 = parent_2;
+        }
+        public Person(string name, string sex, Person parent_1, Person parent_2, Person partner)
+        {
+            this.name = name;
+            this.sex = sex;
+            this.parent_1 = parent_1;
+            this.parent_2 = parent_2;
+            this.partner = partner;
+        }
+        public void PrintParent(Person parent)
+        {
+            switch (parent.sex)
+            {
+                case "F":
+                    Console.WriteLine("Mather: " + parent.name);
+                    break;
+                case "M":
+                    Console.WriteLine("Father: " + parent.name);
+                    break;
+                default:
+                    break;
+            }
+        }
+        public void PrintParents()
+        {
+            if (this.parent_1 != null && this.parent_2 != null)
+            {
+                PrintParent(this.parent_1);
+                PrintParent(this.parent_2);
+            }
+            else Console.WriteLine("No set parents"); 
+        }
+        public void PrintUncle()
+        {
+            Console.WriteLine("Uncle:");
+            foreach (Person person in this.parent_2.parent_1.childrens)
+            {
+                if (person != this.parent_1 && person != this.parent_2)
+                    Console.WriteLine(person.name);
+            }
+            foreach (Person person in this.parent_1.parent_1.childrens)
+            {
+                if (person != this.parent_1 && person != this.parent_2)
+                    Console.WriteLine(person.name);
+            }
+        }
+        public void PrintCousin()
+        {
+            Console.WriteLine("Cousin:");
+            foreach (Person uncle in this.parent_2.parent_1.childrens)
+            {
+                if (uncle != this.parent_1 && uncle != this.parent_2)
+                {
+                    foreach(Person cousin in uncle.childrens)
+                    {
+                        Console.WriteLine(cousin.name);
+                    }
+                }
+            }
+            foreach (Person uncle in this.parent_1.parent_1.childrens)
+            {
+                if (uncle != this.parent_1 && uncle != this.parent_2)
+                {
+                    foreach (Person cousin in uncle.childrens)
+                    {
+                        Console.WriteLine(cousin.name);
+                    }
+                }
+            }
+        }
+        public void PrintInLavs()
+        {
+            Console.WriteLine("In-laws:");
+            Console.WriteLine(this.partner.parent_1.name);
+            Console.WriteLine(this.partner.parent_2.name);
+        }
+
+
+    }
+}
---
+++ Homework1/Homework1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Homework1
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Maria = new Person("Maria", "F");
+            Person Mihail = new Person("Mihail", "M");
+            Maria.partner = Mihail;
+            Mihail.partner = Maria;
+
+            Person Sergey = new Person("Sergey", "M");
+            Person Valentina = new Person("Valentina", "F");
+            Sergey.partner = Valentina;
+            Valentina.partner = Sergey;
+
+            Person Sveta = new Person("Svetlana", "F", Maria, Mihail);
+            Person Andrey = new Person("Andrey", "M", Sergey, Valentina, Sveta);
+            Sveta.partner = Andrey;
+
+            Person Alex = new Person("Alex", "F", Sveta, Andrey);
+
+            Person Anton = new Person("Anton", "M", Sergey, Valentina);
+            Person Nastia = new Person("Nastia", "F", Sergey, Valentina);
+
+            Person Dima = new Person("Dima", "M");
+            Dima.partner = Nastia;
+            Person Misha = new Person("Misha", "M", Dima, Nastia);
+
+            Maria.childrens.Add(Sveta);
+            Mihail.childrens.Add(Sveta);
+            Sveta.childrens.Add(Alex);
+            Andrey.childrens.Add(Alex);
+            Sergey.childrens.Add(Andrey);
+            Sergey.childrens.Add(Anton);
+            Sergey.childrens.Add(Nastia);
+            Valentina.childrens.Add(Andrey);
+            Valentina.childrens.Add(Anton);
+            Valentina.childrens.Add(Nastia);
+            Nastia.childrens.Add(Misha);
+            Dima.childrens.Add(Misha);
+
+            
+            Console.WriteLine("Person:" + Alex.name);
+            Alex.PrintParents();
+            Console.WriteLine('\n');
+
+            Console.WriteLine("Person:" + Maria.name);
+            Maria.PrintParents();
+            Console.WriteLine('\n');
+            
+            Console.WriteLine("Person:" + Alex.name);
+            Alex.PrintUncle();            
+            Alex.PrintCousin();
+            Console.WriteLine('\n');
+
+            Console.WriteLine("Person:" + Andrey.name);
+            Andrey.PrintInLavs();
+        }
+
+    }
+}
